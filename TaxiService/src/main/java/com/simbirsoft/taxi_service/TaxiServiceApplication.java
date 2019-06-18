686c6739eac9fea037b9cb5dd168afb3e62b887a
greenmapc <greenmapc@gmail.com>
2019-06-18 12:49:10 +0300

    init project

---
+++ TaxiService/src/main/java/com/simbirsoft/taxi_service/TaxiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.taxi_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiServiceApplication.class);
+    }
+
+}
---
+++ TaxiService/src/main/resources/templates/index.ftl
@@ -0,0 +1,8 @@
+<html lang="en">
+<head>
+    <title>index</title>
+</head>
+<body>
+    <h1> Hello world! </h1>
+</body>
+</html>
