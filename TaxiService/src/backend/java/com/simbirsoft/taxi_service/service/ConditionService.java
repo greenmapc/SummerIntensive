89972d32cf9197d1def24449f48d9c2843072424
Rustem <ruggaleev@stud.kpfu.ru>
2019-06-26 00:12:27 +0300

    filter & pagination for autos and drivers

--- TaxiService/src/backend/java/com/simbirsoft/taxi_service/service/ConditionService.java
+++
@@ -1,9 +0,0 @@
-package com.simbirsoft.taxi_service.service;
-
-import com.simbirsoft.taxi_service.repository.filters.Condition;
-
-import java.util.List;
-
-public interface ConditionService {
-    List<Condition> getConditionsWithoutOperation(List<String> conditions);
-}
